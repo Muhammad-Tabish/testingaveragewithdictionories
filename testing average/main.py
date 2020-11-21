2fbe77010327340625b280ee180c409f8003433c
tabish hameed <tabishhameed34@gmail.com>
2020-11-21 16:41:05 +0000

    add class

--- testing average/main.py
+++ testing average/main.py
@@ -6,5 +6,18 @@ my_student = {
 
 def average_sum(student):
    return sum(student['grades']) / len(student['grades'])
-print(average_sum(my_student))
+
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.grades = new_grades
+    def average_sum(self):
+        return sum(self.name) / len(self.grades)
+
+student_one = Student('tabish',[55,66,77,888])
+
+student_two = Student('aziz' ,[44,55,66,77])    
+    
+print(student_one.name)
+print(student_two.grades)
   
