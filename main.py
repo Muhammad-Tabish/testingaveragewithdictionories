b6332f124c004cee9362b66f5f9686858d8b7649
tabish hameed <tabishhameed34@gmail.com>
2020-11-22 19:21:38 +0000

    call average method in the class

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-my_movie = {
-    'name': 'The Matrix',
-    'director' : 'Wachowski'
-}
-class Movie:
-    def __init__(self, movie_name, movie_director):
-        self.name = movie_name
-        self.director = movie_director
-my_movie =  Movie('The Matrix', 'Wachowski')
+my_student = {
+"name " : "tabish",
+"grades": [10, 23, 50]
 
-print
+}
+
+def average_sum(student):
+   return sum(student['grades']) / len(student['grades'])
+
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.grades = new_grades
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+student_one = Student('tabish',[55,66,77,888])
+student_two = Student('aziz' ,[44,55,66,77])    
+
+print(student_two.name)
+print(student_two.average())
--- testing average/main.py
+++ testing average/main.py
@@ -11,13 +11,11 @@ class Student:
     def __init__(self, new_name, new_grades):
         self.name = new_name
         self.grades = new_grades
-    def average_sum(self):
-        return sum(self.name) / len(self.grades)
+    def average(self):
+        return sum(self.grades) / len(self.grades)
 
 student_one = Student('tabish',[55,66,77,888])
-
 student_two = Student('aziz' ,[44,55,66,77])    
-    
-print(student_one.name)
-print(student_two.grades)
-  
+
+print(student_two.name)
+print(student_two.average())
